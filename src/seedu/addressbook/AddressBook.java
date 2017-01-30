37ea8ead4a9bc2c93457a799cb83bd7f97c1304d
apoorva17 <apoorva_ullas@yahoo.com.sg>
2017-01-30 12:57:45 +0800

    Extract run method

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -210,13 +210,17 @@ public class AddressBook {
         showWelcomeMessage();
         processProgramArgs(args);
         loadDataFromStorage();
-        while (true) {
+        run();
+    }
+
+	private static void run() {
+		while (true) {
             String userCommand = getUserInput();
             echoUserCommand(userCommand);
             String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
         }
-    }
+	}
 
     /*
      * NOTE : =============================================================
